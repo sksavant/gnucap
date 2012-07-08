59c4657e91c43c9e3ec5e7a5990863fa53a5474f
Savant Krishna <savant.2020@gmail.com>
2012-07-08 21:20:14 +0530

    Editing d_component.cc : not working : To add 3 classes like d_subckt?

--- src/d_component.cc
+++ src/d_component.cc
@@ -18,8 +18,6 @@
  * 02110-1301, USA.
  *------------------------------------------------------------------
  */
-#ifndef D_COMPONENT
-#define D_COMPONENT
 #include "d_subckt.h"
 /*--------------------------------------------------------------------------*/
 #define PORTS_PER_DEVICE 100
@@ -28,7 +26,7 @@ class DEV_COMPONENT : public COMPONENT {
 private:
     explicit DEV_COMPONENT(const DEV_COMPONENT &p) :COMPONENT(p){}
 public:
-    explicit DEV_COMPONENT(); COMPONENT(){}
+    explicit DEV_COMPONENT(): COMPONENT(){}
 protected:
     PARAM_LIST* _params;
 private:
@@ -36,11 +34,11 @@ private:
     std::string param_name(int, int) const;
     void	set_param_by_name(std::string, std::string);
     void	set_param_by_index(int, std::string, int);
-    bool 	param_is_printable(int);
+    bool 	param_is_printable(int) const;
     std::string	param_value(int) const;
 public:
   char      id_letter()const    {untested(); return '\0';}
-  CARD*     clone_instance()const;
+  //CARD*     clone_instance()const;
   bool      print_type_in_spice()const {unreachable(); return false;}
   std::string value_name()const   {incomplete(); return "";}
   std::string dev_type()const     {untested(); return "";}
@@ -57,7 +55,7 @@ public:
   void      map_nodes() {}
   CARD_LIST*       scope()      {return subckt();}
   const CARD_LIST* scope()const {return subckt();}
-  int 	param_count()const 	{ return (static_cast<int>(_params->size())) }
+  int 	param_count()const 	{ return (static_cast<int>(_params->size()));}
 
   std::string port_name(int)const {
     return "";
@@ -70,18 +68,18 @@ private:
   static int    _count;
 };
 /*--------------------------------------------------------------------------*/
-void DEV_COMPONENT::set_param_by_name(std::string Name, std::string Value) const
+void DEV_COMPONENT::set_param_by_name(std::string Name, std::string Value)
 {
 	_params->set(Name,Value);
 }
 /*--------------------------------------------------------------------------*/
-std::string DEV_COMPONENT::param_name(int i) 
+std::string DEV_COMPONENT::param_name(int i)const
 {
 	assert(i<DEV_COMPONENT::param_count());
-	return _params->name(DEV_COMPONENT::param_count() - 1 - i); 
+	return _params->name(DEV_COMPONENT::param_count() - 1 - i);
 }
 /*--------------------------------------------------------------------------*/
-std::string DEV_COMPONENT::param_value(int i, int j) 
+std::string DEV_COMPONENT::param_name(int i, int j)const
 {
 	assert(i<DEV_COMPONENT::param_count());
 	if (j == 0) {
@@ -91,20 +89,26 @@ std::string DEV_COMPONENT::param_value(int i, int j)
 	}
 }
 /*--------------------------------------------------------------------------*/
-std::string DEV_COMPONENT::param_value(int i)const 
+std::string DEV_COMPONENT::param_value(int i)const
 {
 	assert(i<DEV_COMPONENT::param_count());
 	return _params->value(DEV_COMPONENT::param_count() - 1 - i);
 }
 /*--------------------------------------------------------------------------*/
-void DEV_COMPONENT::param_is_printable(int i)const
+bool DEV_COMPONENT::param_is_printable(int i)const
 {
 	assert(i<DEV_COMPONENT::param_count());
 	return _params->is_printable(DEV_COMPONENT::param_count() -1 -i);
 }
 /*--------------------------------------------------------------------------*/
+/*CARD* DEV_COMPONENT::clone_instance()const
+{
+    DEV_COMPONENT* new_instance = dynamic_cast<DEV_COMPONENT*>(p1.clone());
+    new_instance-> _parent = this;
+    return new_instance;
+}*/
+/*--------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------*/
 DEV_COMPONENT p1;
 DISPATCHER<CARD>::INSTALL d1(&device_dispatcher, "component", &p1);
 /*--------------------------------------------------------------------------*/
-#endif
